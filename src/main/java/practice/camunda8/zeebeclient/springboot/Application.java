a262a103631844d3c18e613fb85249b33f9fe52a
suneel <suneelbayya@gmail.com>
2026-06-07 09:00:10 +0530

    Initial commit

---
+++ src/main/java/practice/camunda8/zeebeclient/springboot/Application.java
@@ -0,0 +1,13 @@
+package practice.camunda8.zeebeclient.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
